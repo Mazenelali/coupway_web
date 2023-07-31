3d84c439636a3110d183cd13f26850bd2e09b3f3
Mazen El Ali <mazenelali>
2023-08-01 00:08:39 +0300

    nav and some routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,61 @@
-import '../index.css'
+import React, { useState } from "react";
+import { IoMdHome } from "react-icons/io";
+import { HiOutlineSearch } from "react-icons/hi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineHome } from "react-icons/md";
+import { IoMdNotifications } from "react-icons/io";
+import { BsPersonFill } from "react-icons/bs";
+import { MdFavorite } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+    const [activeLink, setActiveLink] = useState("/"); 
 
-    return <nav className=' w-full h-12 bg-light-green' >
+    let NavLinks = [
+        {
+            icon: <MdOutlineHome />,
+            path: "/",
+            active: <IoMdHome />,
+        },
+        {
+            icon: <HiOutlineSearch />,
+            path: "/search",
+            active: <HiOutlineSearch />,
+        },
+        {
+            icon: <IoMdNotificationsOutline />,
+            path: "/notification",
+            active: <IoMdNotifications />,
+        },
+        {
+            icon: <MdOutlineFavoriteBorder />,
+            path: "/favorite",
+            active: <MdFavorite />,
+        },
+        {
+            icon: <BsPerson />,
+            path: "/profile",
+            active: <BsPersonFill />,
+        },
+    ];
 
-    </nav>
-    ;
+    return (
+        <nav className="w-full h-8 flex text-2xl justify-center items-center space-x-10 text-slate-50 bg-light-green">
+            {NavLinks.map((eachLink) => (
+                <NavLink
+                    key={eachLink.path}
+                    to={eachLink.path}
+                    onClick={() => setActiveLink(eachLink.path)}
+                >
+                    {eachLink.path === activeLink
+                        ? eachLink.active
+                        : eachLink.icon}
+                </NavLink>
+            ))}
+        </nav>
+    );
 }
 
 export default Navbar;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,26 @@
+import { HiOutlineSearch } from "react-icons/hi";
+import { VscLocation } from "react-icons/vsc";
 function Search() {
-    return ( 
-    <div className="w-full h-24 bg-light-green ">
-        
-    </div> 
+    return (
+        <div className="w-full flex items-center justify-center h-16 bg-light-green ">
+            <div className="flex items-center w-[80%] h-[35px] mt-2 bg-white shadow-inner shadow-gray-300">
+                <label className="p-2">
+                    <HiOutlineSearch className=" text-light-green text-xl" />
+                </label>
+                <input
+                    type="text"
+                    placeholder="Sea Food Restaurant"
+                    className=" border-none w-[53%] bg-transparent placeholder:text-xs placeholder:opacity-90"
+                />
+                <span className="w-[1.6px] h-[60%] rounded bg-slate-500 opacity-40"></span>
+                <div className=" w-28 h-[35px]  flex items-center">
+                    <span className="p-1">
+                        <VscLocation className=" text-light-green text-xl" />
+                    </span>
+                    <span className=" text-xs opacity-50 text-gray-700">Location</span>
+                </div>
+            </div>
+        </div>
     );
 }
 
